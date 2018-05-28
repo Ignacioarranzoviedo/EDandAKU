39cd9d2d771682d734da6009e64bfbff7ddba6d8
Nacho <Nacho@LAPTOP-M8HGQ6RS>
2018-05-28 19:39:43 +0200

    Add the geckodriver.exe file to repository.

--- EDandAKU/src/test/linkedIn/SearchingMarketingJob.java
+++ EDandAKU/src/test/linkedIn/SearchingMarketingJob.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.support.events.EventFiringWebDriver;
  */
 public class SearchingMarketingJob {
 	public static void main(String[] args) throws Throwable{
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\Nacho\\Desktop\\Automatización\\WEB-DRIVER-BOOK\\geckodriver.exe");
+		System.setProperty("webdriver.gecko.driver","C:\\Users\\Nacho\\git\\LocalSeleniumRepository\\EDandAKU\\resources\\geckodriver.exe");
 		//we instantiate the Firefox implementation of the WebDriver interface (subclass of the RemoteWebDriver class)
 				
 				WebDriver driver = new FirefoxDriver();
